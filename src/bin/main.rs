7e4ea75d7ba1bb7595ccf5ff1e3bae68529180a1
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2023-01-29 21:51:20 +1300

    Move ray_tracing code to library

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,7 +5,7 @@ use rayon::{
     prelude::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator},
     slice::ParallelSliceMut,
 };
-use raytracer::{Hit, Material, Object, Ray};
+use raytracer::{ray_trace, Material, Object, Ray, BOUNCES, SAMPLES_PER_BOUNCE};
 use thallium::{
     math::{Matrix4x4, One, Vector2, Vector3, Zero},
     platform::{Keycode, Surface, SurfaceEvent},
@@ -85,9 +85,6 @@ void main() {
         let texture = renderer.create_texture(size, Pixels::RGBF(&pixels));
         (pixels, texture)
     };
-
-    const SAMPLES_PER_BOUNCE: usize = 2;
-    const BOUNCES: usize = 5;
 
     let mut camera_position: Vector3<f32> = (0.0, 1.4, -2.0).into();
     let camera_right: Vector3<f32> = (1.0, 0.0, 0.0).into();
@@ -206,74 +203,6 @@ void main() {
                         .map(move |(x, pixel)| (x, y, pixel))
                 })
                 .for_each(|(x, y, pixel)| {
-                    fn get_closest_object(ray: Ray, objects: &[Object]) -> Option<Hit> {
-                        objects.iter().fold(None, |hit, object| {
-                            let new_hit = object.intersect(ray);
-                            hit.zip(new_hit).map_or_else(
-                                || hit.or(new_hit),
-                                |(hit, new_hit)| {
-                                    if hit.distance < new_hit.distance {
-                                        Some(hit)
-                                    } else {
-                                        Some(new_hit)
-                                    }
-                                },
-                            )
-                        })
-                    }
-
-                    fn ray_trace(
-                        ray: Ray,
-                        objects: &[Object],
-                        rng: &mut dyn rand::RngCore,
-                        depth: usize,
-                    ) -> Vector3<f32> {
-                        if depth == 0 {
-                            return Vector3::zero();
-                        }
-
-                        let hit = get_closest_object(ray, objects);
-
-                        if let Some(hit) = hit {
-                            fn random_in_direction(
-                                rng: &mut dyn rand::RngCore,
-                                direction: Vector3<f32>,
-                            ) -> Vector3<f32> {
-                                let random = Vector3 {
-                                    x: rng.gen::<f32>() * 2.0 - 1.0,
-                                    y: rng.gen::<f32>() * 2.0 - 1.0,
-                                    z: rng.gen::<f32>() * 2.0 - 1.0,
-                                };
-                                random * random.dot(direction).signum().into()
-                            }
-
-                            let direction = ray.direction.reflect(hit.normal);
-
-                            let mut in_color: Vector3<f32> = Vector3::zero();
-                            for _ in 0..SAMPLES_PER_BOUNCE {
-                                in_color += ray_trace(
-                                    Ray {
-                                        origin: hit.position + hit.normal * 0.001.into(),
-                                        direction: random_in_direction(rng, direction)
-                                            * (1.0 - hit.material.reflectiveness).into()
-                                            + direction * hit.material.reflectiveness.into(),
-                                    },
-                                    objects,
-                                    rng,
-                                    depth - 1,
-                                );
-                            }
-                            in_color *= (1.0 / SAMPLES_PER_BOUNCE as f32).into();
-
-                            hit.material.emit_color + hit.material.diffuse_color * in_color
-                        } else {
-                            let t = ray.direction.y * 0.5 + 0.5;
-                            let up_color: Vector3<f32> = (1.0, 1.0, 1.0).into();
-                            let down_color: Vector3<f32> = (0.5, 0.7, 1.0).into();
-                            up_color * (1.0 - t).into() + down_color * t.into()
-                        }
-                    }
-
                     let mut rng = rand::thread_rng();
 
                     let mut color = Vector3::<f32>::zero();
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use thallium::math::Vector3;
+use rand::Rng;
+use thallium::math::{Vector3, Zero};
 
 #[derive(Clone, Copy)]
 pub struct Ray {
@@ -93,5 +94,76 @@ impl Object {
                 })
             }
         }
+    }
+}
+
+pub const SAMPLES_PER_BOUNCE: usize = 2;
+pub const BOUNCES: usize = 5;
+
+pub fn get_closest_object(ray: Ray, objects: &[Object]) -> Option<Hit> {
+    objects.iter().fold(None, |hit, object| {
+        let new_hit = object.intersect(ray);
+        hit.zip(new_hit).map_or_else(
+            || hit.or(new_hit),
+            |(hit, new_hit)| {
+                if hit.distance < new_hit.distance {
+                    Some(hit)
+                } else {
+                    Some(new_hit)
+                }
+            },
+        )
+    })
+}
+
+pub fn ray_trace(
+    ray: Ray,
+    objects: &[Object],
+    rng: &mut dyn rand::RngCore,
+    depth: usize,
+) -> Vector3<f32> {
+    if depth == 0 {
+        return Vector3::zero();
+    }
+
+    let hit = get_closest_object(ray, objects);
+
+    if let Some(hit) = hit {
+        fn random_in_direction(
+            rng: &mut dyn rand::RngCore,
+            direction: Vector3<f32>,
+        ) -> Vector3<f32> {
+            let random = Vector3 {
+                x: rng.gen::<f32>() * 2.0 - 1.0,
+                y: rng.gen::<f32>() * 2.0 - 1.0,
+                z: rng.gen::<f32>() * 2.0 - 1.0,
+            };
+            random * random.dot(direction).signum().into()
+        }
+
+        let direction = ray.direction.reflect(hit.normal);
+
+        let mut in_color: Vector3<f32> = Vector3::zero();
+        for _ in 0..SAMPLES_PER_BOUNCE {
+            in_color += ray_trace(
+                Ray {
+                    origin: hit.position + hit.normal * 0.001.into(),
+                    direction: random_in_direction(rng, direction)
+                        * (1.0 - hit.material.reflectiveness).into()
+                        + direction * hit.material.reflectiveness.into(),
+                },
+                objects,
+                rng,
+                depth - 1,
+            );
+        }
+        in_color *= (1.0 / SAMPLES_PER_BOUNCE as f32).into();
+
+        hit.material.emit_color + hit.material.diffuse_color * in_color
+    } else {
+        let t = ray.direction.y * 0.5 + 0.5;
+        let up_color: Vector3<f32> = (1.0, 1.0, 1.0).into();
+        let down_color: Vector3<f32> = (0.5, 0.7, 1.0).into();
+        up_color * (1.0 - t).into() + down_color * t.into()
     }
 }
